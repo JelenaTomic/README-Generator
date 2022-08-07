82865cd9c5c2b456b1c1874a0830f423402c1232
JelenaTomic <tomic.elena09@gmail.com>
2022-08-07 16:10:57 -0500

    added  license

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if(license === 'IBM'){
+    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]';
+ }
+  if(license ===  'ISC'){
+     return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]';
+  }
+  if(license ===  'MIT'){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+ }
+  else {return '' }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
