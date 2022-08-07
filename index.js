44a386aa63fc120b18904db643b74488e453f4bb
JelenaTomic <tomic.elena09@gmail.com>
2022-08-07 16:07:33 -0500

    created a function to write readme file

--- index.js
+++ index.js
@@ -125,10 +125,19 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err =>
+        err ? console.log(err) : console.log('Success!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (data) {
+        console.log(generateMarkdown(data))
+        writeToFile('./README.md', generateMarkdown(data))
+    });
+};
 
 // Function call to initialize app
 init();
